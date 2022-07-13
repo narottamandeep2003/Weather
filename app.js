35bf8939eeea87480126bf841fc4a2576c654319
mandeep <narotta2003@gmail.com>
2022-07-13 16:23:47 +0530

    change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = 80;
+const port = process.env.PORT ||8000;
 
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, './static')))
